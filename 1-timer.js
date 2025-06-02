88a27c0a592c8ae4cb1b7876ad0b35502188b473
Dima2Kravets <dima2.kravets@gmail.com>
2025-06-02 14:00:16 +0000

    Deploying to gh-pages from @ Dima2Kravets/goit-js-hw-10@e3497014954defbbe524612aa1486e51be4136ef 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as s}from"./assets/vendor-BbbuE1sJ.js";const u=document.querySelector("#datetime-picker"),a={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(o){console.log(o[0])}};s(u,a);function t(o){const e=Math.floor(o/864e5),n=Math.floor(o%864e5/36e5),c=Math.floor(o%864e5%36e5/6e4),r=Math.floor(o%864e5%36e5%6e4/1e3);return{days:e,hours:n,minutes:c,seconds:r}}console.log(t(2e3));console.log(t(14e4));console.log(t(2414e4));
+//# sourceMappingURL=1-timer.js.map
---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i}from"./assets/vendor-BbbuE1sJ.js";i.show({title:"✅",message:`Fulfilled promise in ${delay}ms`});
+//# sourceMappingURL=2-snackbar.js.map
